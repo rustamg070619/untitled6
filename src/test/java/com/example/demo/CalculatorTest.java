e8c6d03d2ef3b185ada8c8b476f2c4341fc5bcc5
Rustam <rustamg070619@gmail.com>
2021-01-31 18:43:17 +0300

    test3

---
+++ src/test/java/com/example/demo/CalculatorTest.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+class CalculatorTest {
+    @Autowired
+    private Calculator calculator;
+
+    @Test
+    void shouldAdd() {
+        assertThat(calculator.add(2, 2)).isEqualTo(4);
+    }
+}
